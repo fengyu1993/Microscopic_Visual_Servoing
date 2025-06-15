26fa50dd52b8b0fb93ae6acd9884279fea48f0e4
fengyu1993 <chenyuhan19930920@163.com>
2025-06-15 17:25:13 +0800

    Signed-off-by: fengyu1993 <chenyuhan19930920@163.com>

---
+++ dependence/pylon/include/pylon/PylonVersionNumber.h
@@ -0,0 +1,15 @@
+// This file has been generated automatically using:
+// "${buildin.inputfile}"
+// DO NOT EDIT!
+
+#pragma once
+
+#define PYLON_VERSION_MAJOR           9
+#define PYLON_VERSION_MINOR           0
+#define PYLON_VERSION_SUBMINOR        3
+#define PYLON_VERSION_BUILD           215
+#define PYLON_VERSIONSTRING_MAJOR     "9"
+#define PYLON_VERSIONSTRING_MINOR     "0"
+#define PYLON_VERSIONSTRING_SUBMINOR  "3"
+#define PYLON_VERSIONSTRING_BUILD     "215"
+#define PYLON_VERSIONSTRING_EXTENSION "Internal"
